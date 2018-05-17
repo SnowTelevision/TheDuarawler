52938951b5af005007ac5a56d6d710c269e539ea
SnowTV <pguan100@gmail.com>
2018-05-16 21:04:02 -0400

    Change the sliding door's behavior so that if the door is opened by a key, it won't close unless there is no player/NPC near it and the duration that the door should stay open expired.

--- Assets/Scripts/Environment/Interactable/SlidingDoor.cs
+++ Assets/Scripts/Environment/Interactable/SlidingDoor.cs
@@ -12,9 +12,12 @@ public class SlidingDoor : MonoBehaviour
     public bool requirePass; // Does this door needs the player to put in a passcode to open it
     public int passcode; // The passcode this door is check againest when the player enters it
     public bool openLeft; // Does this door slides towards the negative(left) local x direction or the positive(right) local x direction
+    public float doorKeepOpenDuration; // How long the door will stay open after it is opened by a key or pass code
 
     public Coroutine controlDoorAnimationCoroutine; // The coroutine that runs the door's open&close animation
-    public List<KeyInfo> touchingKeys; // The keys that are currently touching the door
+    public float lastKeyPassEnterTime; // The latest time a correct key or pass code is used on this door
+    //public List<KeyInfo> touchingKeys; // The keys that are currently touching the door
+    public bool isBodyAround; // Is there a body (player or NPC) close to the door
 
     // Use this for initialization
     void Start()
@@ -28,17 +31,27 @@ public class SlidingDoor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        foreach (KeyInfo k in touchingKeys)
+        //foreach (KeyInfo k in touchingKeys)
+        //{
+        //    // Remove the key from the touching key list if the key is no longer holding by a character
+        //    if (!k.GetComponent<ItemInfo>().isBeingHeld)
+        //    {
+        //        touchingKeys.Remove(k);
+        //        // If the key is the last key that's being dropped then close the door
+        //        if(touchingKeys.Count == 0)
+        //        {
+        //            controlDoorAnimationCoroutine = StartCoroutine(GetComponentsInChildren<LinearObjectMovement>()[1].Animate());
+        //        }
+        //    }
+        //}
+
+        // If the door requires key or pass code to open
+        if (requireKey || requirePass)
         {
-            // Remove the key from the touching key list if the key is no longer holding by a character
-            if (!k.GetComponent<ItemInfo>().isBeingHeld)
+            // If a body is not close to the door and the duration of the door should keep open passed then close the door
+            if (Time.time - lastKeyPassEnterTime >= doorKeepOpenDuration && !isBodyAround)
             {
-                touchingKeys.Remove(k);
-                // If the key is the last key that's being dropped then close the door
-                if(touchingKeys.Count == 0)
-                {
-                    controlDoorAnimationCoroutine = StartCoroutine(GetComponentsInChildren<LinearObjectMovement>()[1].Animate());
-                }
+                controlDoorAnimationCoroutine = StartCoroutine(GetComponentsInChildren<LinearObjectMovement>()[1].Animate());
             }
         }
     }
@@ -47,15 +60,31 @@ public class SlidingDoor : MonoBehaviour
     {
         bool openDoor = false;
 
+        // If the player is close to the door
+        if (other.gameObject.layer == LayerMask.NameToLayer("PlayerBody"))
+        {
+            isBodyAround = true;
+        }
+
         if (requireKey)
         {
+            //// If the item is a key and is currently holding by the player or some NPC
+            //if (other.GetComponent<KeyInfo>() && other.GetComponent<ItemInfo>().isBeingHeld)
+            //{
+            //    if (other.GetComponent<KeyInfo>().keyCode == keyCode)
+            //    {
+            //        openDoor = DetectIfUseKey();
+            //        touchingKeys.Add(other.GetComponent<KeyInfo>());
+            //    }
+            //}
             // If the item is a key and is currently holding by the player or some NPC
             if (other.GetComponent<KeyInfo>() && other.GetComponent<ItemInfo>().isBeingHeld)
             {
+                // If the key code match with the door
                 if (other.GetComponent<KeyInfo>().keyCode == keyCode)
                 {
-                    openDoor = DetectIfUseKey();
-                    touchingKeys.Add(other.GetComponent<KeyInfo>());
+                    openDoor = true;
+                    lastKeyPassEnterTime = Time.time;
                 }
             }
         }
@@ -86,39 +115,45 @@ public class SlidingDoor : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        // If this door needs a key to open and a key touching it is just picked up by someone
-        if (requireKey && other.GetComponent<KeyInfo>() &&
-            other.GetComponent<KeyInfo>().keyCode == keyCode &&
-            !touchingKeys.Contains(other.GetComponent<KeyInfo>()) &&
-            other.GetComponent<ItemInfo>().isBeingHeld)
-        {
-            // If the key is the only key touching it
-            if (DetectIfUseKey())
-            {
-                controlDoorAnimationCoroutine = StartCoroutine(GetComponentsInChildren<LinearObjectMovement>()[0].Animate());
-            }
-            // Add the key back to the touching key list
-            touchingKeys.Add(other.GetComponent<KeyInfo>());
-        }
+        //// If this door needs a key to open and a key touching it is just picked up by someone
+        //if (requireKey && other.GetComponent<KeyInfo>() &&
+        //    other.GetComponent<KeyInfo>().keyCode == keyCode &&
+        //    !touchingKeys.Contains(other.GetComponent<KeyInfo>()) &&
+        //    other.GetComponent<ItemInfo>().isBeingHeld)
+        //{
+        //    // If the key is the only key touching it
+        //    if (DetectIfUseKey())
+        //    {
+        //        controlDoorAnimationCoroutine = StartCoroutine(GetComponentsInChildren<LinearObjectMovement>()[0].Animate());
+        //    }
+        //    // Add the key back to the touching key list
+        //    touchingKeys.Add(other.GetComponent<KeyInfo>());
+        //}
     }
 
     private void OnTriggerExit(Collider other)
     {
         bool closeDoor = false;
 
+        // If the player is close to the door
+        if (other.gameObject.layer == LayerMask.NameToLayer("PlayerBody"))
+        {
+            isBodyAround = false;
+        }
+
         if (requireKey)
         {
-            // If the item is a key and is currently holding by the player or some NPC
-            if (other.GetComponent<KeyInfo>() && other.GetComponent<ItemInfo>().isBeingHeld)
-            {
-                // If the key code match with the door
-                if (other.GetComponent<KeyInfo>().keyCode == keyCode)
-                {
-                    // Remove the key from the touching key list
-                    touchingKeys.Remove(other.GetComponent<KeyInfo>());
-                    closeDoor = DetectIfUseKey();
-                }
-            }
+            //// If the item is a key and is currently holding by the player or some NPC
+            //if (other.GetComponent<KeyInfo>() && other.GetComponent<ItemInfo>().isBeingHeld)
+            //{
+            //    // If the key code match with the door
+            //    if (other.GetComponent<KeyInfo>().keyCode == keyCode)
+            //    {
+            //        // Remove the key from the touching key list
+            //        touchingKeys.Remove(other.GetComponent<KeyInfo>());
+            //        closeDoor = DetectIfUseKey();
+            //    }
+            //}
         }
         else if (requirePass)
         {
@@ -145,19 +180,19 @@ public class SlidingDoor : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// When a key start/stop touching the door, detect if anything happenes to the door
-    /// </summary>
-    /// <returns></returns>
-    public bool DetectIfUseKey()
-    {
-        if (touchingKeys.Count == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    ///// <summary>
+    ///// When a key start/stop touching the door, detect if anything happenes to the door
+    ///// </summary>
+    ///// <returns></returns>
+    //public bool DetectIfUseKey()
+    //{
+    //    if (touchingKeys.Count == 0)
+    //    {
+    //        return true;
+    //    }
+    //    else
+    //    {
+    //        return false;
+    //    }
+    //}
 }
